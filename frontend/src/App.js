58a8a582d902fe53cc16f9e50e8bd6f88116bc1e
michael <mikemarlpen10334@gmail.com>
2022-06-07 23:48:14 +0800

    6/7

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 const App = () => {
   return (
     <>
-      <h1>ecproject</h1>
+      <Header />
+      <main>
+        <Container>
+          <h1>ecproject</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   )
 }
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,20 @@
+import { Nav, Navbar, Container } from 'react-bootstrap'
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">ecoproject</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/cart">Cart</Nav.Link>
+              <Nav.Link href="/login">Sign In</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+export default Header
