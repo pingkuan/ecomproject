1d695a1ff2e66ebc13eb45ca9d5fe5e5f3432761
michael <mikemarlpen10334@gmail.com>
2022-06-09 02:35:15 +0800

    add images

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,34 @@
-import { Nav, Navbar, Container } from 'react-bootstrap'
+import { Nav, Navbar, Container, NavDropdown } from 'react-bootstrap'
+import { useState } from 'react'
+
 const Header = () => {
+  const [show, setShow] = useState(false)
+  const showDropdown = (e) => {
+    setShow(!show)
+  }
+  const hideDropdown = (e) => {
+    setShow(false)
+  }
   return (
     <header>
-      <Navbar bg="dark" expand="lg">
+      <Navbar bg="dark" variant="light" expand="lg">
         <Container>
           <Navbar.Brand href="/">ecoproject</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart">Cart</Nav.Link>
-              <Nav.Link href="/login">Sign In</Nav.Link>
+              <Nav.Link href="/cart">
+                <i className="fas fa-shopping-cart"></i>Cart
+              </Nav.Link>
+              <NavDropdown
+                title="Sign in"
+                id="collasible-nav-dropdown"
+                show={show}
+                onMouseEnter={showDropdown}
+                onMouseLeave={hideDropdown}
+              >
+                Sign in
+              </NavDropdown>
             </Nav>
           </Navbar.Collapse>
         </Container>
