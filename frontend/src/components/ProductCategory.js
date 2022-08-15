7945f7225ea87155771594eae106eab5161901a2
pingkuan <mikemarlpen10334@gmail.com>
2022-08-15 22:51:00 +0800

    use linkcontainer to productcategory

--- frontend/src/components/ProductCategory.js
+++ frontend/src/components/ProductCategory.js
@@ -1,25 +1,29 @@
 import { Card, Button } from 'react-bootstrap';
-
+import { LinkContainer } from 'react-router-bootstrap';
 const ProductCategory = ({ category, image, name }) => {
   return (
-    <Card
-      className='text-center w-100 galleryBox text-decoration-none cardHover'
-      as='a'
-      href={`/${category}/page`}
-    >
-      <Card.Img src={image} variant='top' className=' img-fluid' />
-      <Card.Body className='d-flex flex-column'>
-        <Card.Title as='h3'>
-          <strong>{name}</strong>
-        </Card.Title>
-        <Button
-          type='button'
-          className='btn btn-primary align-self-center btn-sm'
+    <>
+      <LinkContainer to={`/${category}/page`}>
+        <Card
+          className='text-center w-100 galleryBox text-decoration-none cardHover'
+          as='a'
+          href={`/${category}/page`}
         >
-          查詢所有
-        </Button>
-      </Card.Body>
-    </Card>
+          <Card.Img src={image} variant='top' className=' img-fluid' />
+          <Card.Body className='d-flex flex-column'>
+            <Card.Title as='h3'>
+              <strong>{name}</strong>
+            </Card.Title>
+            <Button
+              type='button'
+              className='btn btn-primary align-self-center btn-sm'
+            >
+              查詢所有
+            </Button>
+          </Card.Body>
+        </Card>
+      </LinkContainer>
+    </>
   );
 };
 export default ProductCategory;
